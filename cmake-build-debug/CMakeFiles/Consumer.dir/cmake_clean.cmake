507a5f97a7033cfc1839419ee6d17006fb48b77a
Hayam Hiba <hayam.hiba.ext@brightskiesinc.com>
2022-12-16 17:47:47 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/Consumer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Consumer.dir/consumer.cpp.o"
+  "CMakeFiles/Consumer.dir/consumer.cpp.o.d"
+  "Consumer"
+  "Consumer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Consumer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Producer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Producer.dir/producer.cpp.o"
+  "CMakeFiles/Producer.dir/producer.cpp.o.d"
+  "Producer"
+  "Producer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Producer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
